e4b9f2ccebcdd6a4d35e8c4188295664907d6547
max.ross <max.ross@ea1b383e-165b-11de-8120-8d224146dbbc>
2009-07-28 00:31:16 +0000

    Tests Serializing a List using @Lob

--- tests/org/datanucleus/store/appengine/JPALobTest.java
+++ tests/org/datanucleus/store/appengine/JPALobTest.java
@@ -24,6 +24,7 @@ import com.google.appengine.api.datastore.Text;
 import org.datanucleus.test.HasLob;
 
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * @author Max Ross <maxr@google.com>
@@ -35,6 +36,8 @@ public class JPALobTest extends JPATestCase {
 
     pojo.setBigString("a really big string");
     pojo.setBigByteArray("a really big byte array".getBytes());
+    Date now = new Date();
+    pojo.setDateList(Utils.newArrayList(now));
 
     beginTxn();
     em.persist(pojo);
@@ -43,12 +46,17 @@ public class JPALobTest extends JPATestCase {
     Entity e = ldth.ds.get(KeyFactory.createKey(HasLob.class.getSimpleName(), pojo.getId()));
     assertEquals(new Text("a really big string"), e.getProperty("bigString"));
     assertEquals(new Blob("a really big byte array".getBytes()), e.getProperty("bigByteArray"));
+    assertEquals(new Blob(SerializationManager.DEFAULT_SERIALIZATION_STRATEGY.serialize(
+        Utils.newArrayList(now)).getBytes()), e.getProperty("dateList"));
   }
 
   public void testFetch() {
     Entity e = new Entity(HasLob.class.getSimpleName());
     e.setProperty("bigString", new Text("a really big string"));
     e.setProperty("bigByteArray", new Blob("a really big byte array".getBytes()));
+    Date now = new Date();
+    e.setProperty("dateList", new Blob(SerializationManager.DEFAULT_SERIALIZATION_STRATEGY.serialize(
+        Utils.newArrayList(now)).getBytes()));
     ldth.ds.put(e);
 
     beginTxn();
@@ -56,5 +64,6 @@ public class JPALobTest extends JPATestCase {
     assertNotNull(pojo);
     assertEquals("a really big string", pojo.getBigString());
     assertTrue(Arrays.equals("a really big byte array".getBytes(), pojo.getBigByteArray()));
+    assertEquals(Utils.newArrayList(now), pojo.getDateList());
   }
 }
