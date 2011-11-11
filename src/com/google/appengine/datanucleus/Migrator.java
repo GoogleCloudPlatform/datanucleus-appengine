7e0395eed1588f49fd3b1055bb428ef76e8d3e7e
andy_jefferson <andy_jefferson@yahoo.com>
2011-11-11 07:39:31 +0000

    Add 2 migrate() methods, one taking the class and Iterable<Entity> and the other taking the class names.

--- src/com/google/appengine/datanucleus/Migrator.java
+++ src/com/google/appengine/datanucleus/Migrator.java
@@ -78,171 +78,235 @@ public class Migrator {
   }
 
   /**
+   * Method to migrate the provided Entities of the specified class.
+   * Does a single PUT of all changed Entity objects.
+   * @param cls Class
+   * @param iter Iterator for the Entity objects
+   */
+  public static void migrate(Class cls, Iterable<Entity> iter) {
+    NucleusContext nucCtx = new NucleusContext("JDO", null);
+    PersistenceConfiguration propConfig = nucCtx.getPersistenceConfiguration();
+
+    Properties props = new Properties();
+    props.setProperty("datanucleus.ConnectionURL", "appengine");
+    propConfig.setPersistenceProperties(props);
+
+    DatastoreManager storeMgr = (DatastoreManager) nucCtx.getStoreManager();
+    ClassLoaderResolver clr = nucCtx.getClassLoaderResolver(null);
+    AbstractClassMetaData cmd = nucCtx.getMetaDataManager().getMetaDataForClass(cls, clr);
+
+    Set<Entity> changedEntities = new HashSet<Entity>();
+    int[] relationFieldNumbers = cmd.getRelationMemberPositions(clr, nucCtx.getMetaDataManager());
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    if (relationFieldNumbers != null && relationFieldNumbers.length > 0) {
+      for (Entity entity : iter) {
+        // For each Entity, process the owner fields
+        boolean changed = migrateEntity(nucCtx, cls, entity, cmd, clr, storeMgr);
+        if (changed) {
+          changedEntities.add(entity);
+        }
+      }
+    }
+    
+    if (!changedEntities.isEmpty()) {
+      // PUT the updated entities
+      NucleusLogger.DATASTORE_NATIVE.debug("Putting " + changedEntities.size() + " entities of class " + cls.getName());
+      datastore.put(changedEntities);
+    }
+
+    nucCtx.close();
+  }
+
+  /**
+   * Convenience method to migrate an Entity. Adds any necessary properties containing the child keys.
+   * Returns whether it has been updated. The Entity is not PUT in the datastore in this method.
+   * @param nucCtx NucleusContext
+   * @param cls Class
+   * @param entity Entity
+   * @param cmd Metadata for the class
+   * @param clr ClassLoader resolver
+   * @param storeMgr Store Manager
+   * @return Whether the entity is updated
+   */
+  protected static boolean migrateEntity(NucleusContext nucCtx, Class cls, Entity entity,
+      AbstractClassMetaData cmd, ClassLoaderResolver clr, DatastoreManager storeMgr) {
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    boolean changed = false;
+
+    int[] relationFieldNumbers = cmd.getRelationMemberPositions(clr, nucCtx.getMetaDataManager());
+    if (relationFieldNumbers == null || relationFieldNumbers.length == 0) {
+      return false;
+    }
+
+    // Process any relation owner fields
+    NucleusLogger.GENERAL.info(">> Migrating Entity with key=" + entity.getKey());
+    for (int i=0;i<relationFieldNumbers.length;i++) {
+      AbstractMemberMetaData mmd = cmd.getMetaDataForManagedMemberAtAbsolutePosition(relationFieldNumbers[i]);
+      if (MetaDataUtils.isOwnedRelation(mmd)) {
+        int relationType = mmd.getRelationType(clr);
+        if (relationType == Relation.ONE_TO_ONE_UNI ||
+            (relationType == Relation.ONE_TO_ONE_BI && mmd.getMappedBy() == null)) {
+          // 1-1 owner
+          String propName = EntityUtils.getPropertyName(storeMgr.getIdentifierFactory(), mmd);
+          if (!entity.hasProperty(propName)) {
+            // Add property in Entity with child key
+            AbstractClassMetaData relCmd = nucCtx.getMetaDataManager().getMetaDataForClass(mmd.getTypeName(), clr);
+            String relKindName = EntityUtils.getKindName(storeMgr.getIdentifierFactory(), relCmd);
+            Query q2 = new Query(relKindName, entity.getKey());
+            PreparedQuery pq2 = datastore.prepare(q2);
+            Object value = null;
+            for (Entity childEntity : pq2.asIterable()) { // Should be only one (see FetchFieldManager)
+              if (entity.getKey().equals(childEntity.getKey().getParent())) {
+                value = childEntity.getKey();
+                break;
+              }
+            }
+            changed = true;
+            entity.setProperty(propName, value);
+          }
+        }
+        else if (relationType == Relation.ONE_TO_MANY_UNI || relationType == Relation.ONE_TO_MANY_BI) {
+          // 1-N
+          String propName = EntityUtils.getPropertyName(storeMgr.getIdentifierFactory(), mmd);
+          if (!entity.hasProperty(propName)) {
+            AbstractClassMetaData childCmd = mmd.getCollection().getElementClassMetaData(clr, nucCtx.getMetaDataManager());
+            String childKindName = EntityUtils.getKindName(storeMgr.getIdentifierFactory(), childCmd);
+            Query q = new Query(childKindName, entity.getKey());
+
+            if (List.class.isAssignableFrom(mmd.getType())) {
+              // Add List ordering
+              List<Query.SortPredicate> sortPredicates = Utils.newArrayList();
+              boolean indexedList = true;
+              if (mmd.getOrderMetaData() != null && !mmd.getOrderMetaData().isIndexedList()) {
+                indexedList = false;
+              }
+              if (indexedList) {
+                // Get the index property name
+                String indexPropName = null;
+                OrderMetaData ordmd = mmd.getOrderMetaData();
+                if (ordmd != null) {
+                  String colName;
+                  if (ordmd.getColumnMetaData() != null && ordmd.getColumnMetaData().length > 0
+                      && ordmd.getColumnMetaData()[0].getName() != null) {
+                    // User-defined name so create an identifier using it
+                    colName = ordmd.getColumnMetaData()[0].getName();
+                    indexPropName = storeMgr.getIdentifierFactory().newDatastoreFieldIdentifier(colName).getIdentifierName();
+                  }
+                }
+                if (indexPropName == null) {
+                  // No name defined so generate one
+                  indexPropName = storeMgr.getIdentifierFactory().newForeignKeyFieldIdentifier(
+                      mmd, null, null, true, FieldRole.ROLE_INDEX).getIdentifierName();
+                }
+
+                // Order by the ordering column
+                Query.SortPredicate sortPredicate =
+                  new Query.SortPredicate(indexPropName, Query.SortDirection.ASCENDING);
+                sortPredicates.add(sortPredicate);
+              } else {
+                for (OrderMetaData.FieldOrder fieldOrder : mmd.getOrderMetaData().getFieldOrders()) {
+                  String orderMemberName = fieldOrder.getFieldName();
+                  AbstractMemberMetaData orderMemberMmd = childCmd.getMetaDataForMember(orderMemberName);
+                  String orderPropName = EntityUtils.getPropertyName(storeMgr.getIdentifierFactory(), orderMemberMmd);
+                  boolean isPrimaryKey = orderMemberMmd.isPrimaryKey();
+                  if (isPrimaryKey) {
+                    if (fieldOrder.isForward() && sortPredicates.isEmpty()) {
+                      // Don't even bother adding if the first sort is id ASC (this is the
+                      // default sort so there's no point in making the datastore figure this out).
+                      break;
+                    }
+                    // sorting by id requires us to use a reserved property name
+                    orderPropName = Entity.KEY_RESERVED_PROPERTY;
+                  }
+                  Query.SortPredicate sortPredicate = new Query.SortPredicate(orderPropName,
+                      fieldOrder.isForward() ? Query.SortDirection.ASCENDING : Query.SortDirection.DESCENDING);
+                  sortPredicates.add(sortPredicate);
+                  if (isPrimaryKey) {
+                    // User wants to sort by pk.  Since pk is guaranteed to be unique, break
+                    // because we know there's no point in adding any more sort predicates
+                    break;
+                  }
+                }
+              }
+              for (SortPredicate sp : sortPredicates) {
+                q.addSort(sp.getPropertyName(), sp.getDirection());
+              }
+            }
+
+            PreparedQuery pq = datastore.prepare(q);
+            List<Key> values = new ArrayList<Key>();
+            for (Entity childEntity : pq.asIterable()) { // Should be only one (see FetchFieldManager)
+              if (entity.getKey().equals(childEntity.getKey().getParent())) {
+                values.add(childEntity.getKey());
+              }
+            }
+
+            changed = true;
+            entity.setProperty(propName, values);
+          }
+        }
+      }
+    }
+    return changed;
+  }
+
+  /**
    * Method to migrate all entities for the specified classes to having child keys stored in the parent.
    * @param classNames Name of the classes (fully qualified)
    */
-  public static void migrateClasses(String[] classNames) {
+  public static void migrate(String[] classNames) {
 
     if (classNames != null && classNames.length > 0) {
-      NucleusContext nucleusCtx = new NucleusContext("JDO", null);
-      PersistenceConfiguration propConfig = nucleusCtx.getPersistenceConfiguration();
+      NucleusContext nucCtx = new NucleusContext("JDO", null);
+      PersistenceConfiguration propConfig = nucCtx.getPersistenceConfiguration();
 
       Properties props = new Properties();
       props.setProperty("datanucleus.ConnectionURL", "appengine");
       propConfig.setPersistenceProperties(props);
 
-      for (int i=0;i<classNames.length;i++) {
-        migrateClass(classNames[i], nucleusCtx);
-      }
-
-      nucleusCtx.close();
-    }
-  }
-
-  /**
-   * Method to migrate all entities for the specified class to having child keys stored in the parent.
-   * @param className Name of the class (fully qualified)
-   * @param nucCtx Nucleus Context
-   */
-  public static void migrateClass(String className, NucleusContext nucCtx) {
-    DatastoreManager storeMgr = (DatastoreManager) nucCtx.getStoreManager();
-    ClassLoaderResolver clr = nucCtx.getClassLoaderResolver(null);
-    AbstractClassMetaData cmd = nucCtx.getMetaDataManager().getMetaDataForClass(className, clr);
-    NucleusLogger.GENERAL.info(">> Migrating Entities for " + className);
-
-    Set<Entity> changedEntities = new HashSet<Entity>();
-    int[] relationFieldNumbers = cmd.getRelationMemberPositions(clr, nucCtx.getMetaDataManager());
-    if (relationFieldNumbers != null && relationFieldNumbers.length > 0) {
+      DatastoreManager storeMgr = (DatastoreManager) nucCtx.getStoreManager();
+      ClassLoaderResolver clr = nucCtx.getClassLoaderResolver(null);
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-      // Do a query for the particular kind, restricting it by any discriminator
-      String kindName = EntityUtils.getKindName(storeMgr.getIdentifierFactory(), cmd);
+      for (int i=0;i<classNames.length;i++) {
+        Class cls = clr.classForName(classNames[i]);
+        AbstractClassMetaData cmd = nucCtx.getMetaDataManager().getMetaDataForClass(classNames[i], clr);
+        NucleusLogger.GENERAL.info(">> Migrating Entities for " + classNames[i]);
 
-      // Query for all Entities of this class
-      Query q = new Query(kindName);
-      if (cmd.hasDiscriminatorStrategy()) {
-        String discriminatorPropertyName = EntityUtils.getDiscriminatorPropertyName(storeMgr.getIdentifierFactory(), 
-            cmd.getDiscriminatorMetaDataRoot());
-        Object discrimValue = cmd.getDiscriminatorValue();
-        q.addFilter(discriminatorPropertyName, Query.FilterOperator.EQUAL, discrimValue);
-      }
+        Set<Entity> changedEntities = new HashSet<Entity>();
+        int[] relationFieldNumbers = cmd.getRelationMemberPositions(clr, nucCtx.getMetaDataManager());
+        if (relationFieldNumbers != null && relationFieldNumbers.length > 0) {
 
-      PreparedQuery pq = datastore.prepare(q);
-      for (Entity entity : pq.asIterable()) {
-        // For each Entity, process the owner fields
-        NucleusLogger.GENERAL.info(">> Migrating Entity with key=" + entity);
-        for (int i=0;i<relationFieldNumbers.length;i++) {
-          AbstractMemberMetaData mmd = cmd.getMetaDataForManagedMemberAtAbsolutePosition(relationFieldNumbers[i]);
-          if (MetaDataUtils.isOwnedRelation(mmd)) {
-            int relationType = mmd.getRelationType(clr);
-            if (relationType == Relation.ONE_TO_ONE_UNI ||
-                (relationType == Relation.ONE_TO_ONE_BI && mmd.getMappedBy() == null)) {
-              // 1-1 owner
-              String propName = EntityUtils.getPropertyName(storeMgr.getIdentifierFactory(), mmd);
-              if (!entity.hasProperty(propName)) {
-                // Add property in Entity with child key
-                AbstractClassMetaData relCmd = nucCtx.getMetaDataManager().getMetaDataForClass(mmd.getTypeName(), clr);
-                String relKindName = EntityUtils.getKindName(storeMgr.getIdentifierFactory(), relCmd);
-                Query q2 = new Query(relKindName, entity.getKey());
-                PreparedQuery pq2 = datastore.prepare(q2);
-                Object value = null;
-                for (Entity childEntity : pq2.asIterable()) { // Should be only one (see FetchFieldManager)
-                  if (entity.getKey().equals(childEntity.getKey().getParent())) {
-                    value = childEntity.getKey();
-                    break;
-                  }
-                }
-                entity.setProperty(propName, value);
-              }
+          // Do a query for the particular kind, restricting it by any discriminator
+          String kindName = EntityUtils.getKindName(storeMgr.getIdentifierFactory(), cmd);
+
+          // Query for all Entities of this class
+          Query q = new Query(kindName);
+          if (cmd.hasDiscriminatorStrategy()) {
+            String discriminatorPropertyName = EntityUtils.getDiscriminatorPropertyName(storeMgr.getIdentifierFactory(), 
+                cmd.getDiscriminatorMetaDataRoot());
+            Object discrimValue = cmd.getDiscriminatorValue();
+            q.addFilter(discriminatorPropertyName, Query.FilterOperator.EQUAL, discrimValue);
+          }
+
+          PreparedQuery pq = datastore.prepare(q);
+          for (Entity entity : pq.asIterable()) {
+            boolean changed = migrateEntity(nucCtx, cls, entity, cmd, clr, storeMgr);
+            if (changed) {
+              changedEntities.add(entity);
             }
-            else if (relationType == Relation.ONE_TO_MANY_UNI || relationType == Relation.ONE_TO_MANY_BI) {
-              // 1-N
-              String propName = EntityUtils.getPropertyName(storeMgr.getIdentifierFactory(), mmd);
-              if (!entity.hasProperty(propName)) {
-                AbstractClassMetaData childCmd = mmd.getCollection().getElementClassMetaData(clr, nucCtx.getMetaDataManager());
-                String childKindName = EntityUtils.getKindName(storeMgr.getIdentifierFactory(), childCmd);
-                Query q2 = new Query(childKindName, entity.getKey());
-
-                if (List.class.isAssignableFrom(mmd.getType())) {
-                  // Add List ordering
-                  List<Query.SortPredicate> sortPredicates = Utils.newArrayList();
-                  boolean indexedList = true;
-                  if (mmd.getOrderMetaData() != null && !mmd.getOrderMetaData().isIndexedList()) {
-                    indexedList = false;
-                  }
-                  if (indexedList) {
-                    // Get the index property name
-                    String indexPropName = null;
-                    OrderMetaData ordmd = mmd.getOrderMetaData();
-                    if (ordmd != null) {
-                      String colName;
-                      if (ordmd.getColumnMetaData() != null && ordmd.getColumnMetaData().length > 0
-                          && ordmd.getColumnMetaData()[0].getName() != null) {
-                        // User-defined name so create an identifier using it
-                        colName = ordmd.getColumnMetaData()[0].getName();
-                        indexPropName = storeMgr.getIdentifierFactory().newDatastoreFieldIdentifier(colName).getIdentifierName();
-                      }
-                    }
-                    if (indexPropName == null) {
-                      // No name defined so generate one
-                      indexPropName = storeMgr.getIdentifierFactory().newForeignKeyFieldIdentifier(
-                          mmd, null, null, true, FieldRole.ROLE_INDEX).getIdentifierName();
-                    }
-
-                    // Order by the ordering column
-                    Query.SortPredicate sortPredicate =
-                      new Query.SortPredicate(indexPropName, Query.SortDirection.ASCENDING);
-                    sortPredicates.add(sortPredicate);
-                  } else {
-                    for (OrderMetaData.FieldOrder fieldOrder : mmd.getOrderMetaData().getFieldOrders()) {
-                      String orderMemberName = fieldOrder.getFieldName();
-                      AbstractMemberMetaData orderMemberMmd = childCmd.getMetaDataForMember(orderMemberName);
-                      String orderPropName = EntityUtils.getPropertyName(storeMgr.getIdentifierFactory(), orderMemberMmd);
-                      boolean isPrimaryKey = orderMemberMmd.isPrimaryKey();
-                      if (isPrimaryKey) {
-                        if (fieldOrder.isForward() && sortPredicates.isEmpty()) {
-                          // Don't even bother adding if the first sort is id ASC (this is the
-                          // default sort so there's no point in making the datastore figure this out).
-                          break;
-                        }
-                        // sorting by id requires us to use a reserved property name
-                        orderPropName = Entity.KEY_RESERVED_PROPERTY;
-                      }
-                      Query.SortPredicate sortPredicate = new Query.SortPredicate(orderPropName,
-                          fieldOrder.isForward() ? Query.SortDirection.ASCENDING : Query.SortDirection.DESCENDING);
-                      sortPredicates.add(sortPredicate);
-                      if (isPrimaryKey) {
-                        // User wants to sort by pk.  Since pk is guaranteed to be unique, break
-                        // because we know there's no point in adding any more sort predicates
-                        break;
-                      }
-                    }
-                  }
-                  for (SortPredicate sp : sortPredicates) {
-                    q.addSort(sp.getPropertyName(), sp.getDirection());
-                  }
-                }
-
-                PreparedQuery pq2 = datastore.prepare(q2);
-                List<Key> values = new ArrayList<Key>();
-                for (Entity childEntity : pq2.asIterable()) { // Should be only one (see FetchFieldManager)
-                  if (entity.getKey().equals(childEntity.getKey().getParent())) {
-                    values.add(childEntity.getKey());
-                  }
-                }
-
-                entity.setProperty(propName, values);
-              }
-            }
+          }
+          
+          if (!changedEntities.isEmpty()) {
+            // PUT the updated entities
+            NucleusLogger.DATASTORE_NATIVE.debug("Putting " + changedEntities.size() + " entities of kind " + kindName);
+            datastore.put(changedEntities);
           }
         }
       }
-      
-      if (!changedEntities.isEmpty()) {
-        // PUT the updated entities
-        NucleusLogger.DATASTORE_NATIVE.debug("Putting " + changedEntities.size() + " entities of kind " + kindName);
-        datastore.put(changedEntities);
-      }
+
+      nucCtx.close();
     }
   }
 }
