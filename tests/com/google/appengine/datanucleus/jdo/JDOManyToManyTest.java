1d2d139201eb71c68c9443401427632242a5f969
andy_jefferson <andy_jefferson@yahoo.com>
2012-01-22 17:02:59 +0000

    basic tests for unowned M-N relations

---
+++ tests/com/google/appengine/datanucleus/jdo/JDOManyToManyTest.java
@@ -0,0 +1,88 @@
+/**********************************************************************
+Copyright (c) 2012 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**********************************************************************/
+package com.google.appengine.datanucleus.jdo;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import org.datanucleus.util.NucleusLogger;
+
+import com.google.appengine.datanucleus.test.HasManyToManyAJDO;
+import com.google.appengine.datanucleus.test.HasManyToManyBJDO;
+
+/**
+ * Tests for unowned M-N relations
+ */
+public class JDOManyToManyTest extends JDOTestCase {
+
+  public void testPersist() throws Exception {
+    PersistenceManager pm = pmf.getPersistenceManager();
+    try {
+      // Persist Side A
+      HasManyToManyBJDO b = new HasManyToManyBJDO();
+      pm.makePersistent(b);
+
+      // Create and persist Side B with another Side A, plus the existing Side A
+      HasManyToManyAJDO a = new HasManyToManyAJDO();
+      HasManyToManyBJDO b2 = new HasManyToManyBJDO();
+      b2.getAs().add(a);
+      a.getBs().add(b2);
+      a.getBs().add(b);
+      b.getAs().add(a);
+      pm.makePersistent(b2);
+      pm.makePersistent(a);
+    } catch (Exception e) {
+      NucleusLogger.GENERAL.error("Exception in persist", e);
+      fail("Exception in test : " + e.getMessage());
+      return;
+    } finally {
+      if (pm.currentTransaction().isActive()) {
+        pm.currentTransaction().rollback();
+      }
+      pm.close();
+    }
+    pmf.getDataStoreCache().evictAll();
+
+    pm = pmf.getPersistenceManager();
+    try {
+      Query qa = pm.newQuery(HasManyToManyAJDO.class);
+      List<HasManyToManyAJDO> results1 = (List<HasManyToManyAJDO>) qa.execute();
+      assertNotNull(results1);
+      assertEquals("Incorrect number of side A", 1, results1.size());
+      HasManyToManyAJDO a = results1.iterator().next();
+      Set<HasManyToManyBJDO> bs = a.getBs();
+      assertNotNull(bs);
+      assertEquals("Incorrect number of side B for A", 2, bs.size());
+
+      Query qb = pm.newQuery(HasManyToManyBJDO.class);
+      List<HasManyToManyBJDO> results2 = (List<HasManyToManyBJDO>) qb.execute();
+      assertNotNull(results2);
+      assertEquals("Incorrect number of side B", 2, results2.size());
+    } catch (Exception e) {
+      NucleusLogger.GENERAL.error("Exception in retrieval/checking", e);
+      fail("Exception in test : " + e.getMessage());
+      return;
+    } finally {
+      if (pm.currentTransaction().isActive()) {
+        pm.currentTransaction().rollback();
+      }
+      pm.close();
+    }
+  }
+}
---
+++ tests/com/google/appengine/datanucleus/jpa/JPAManyToManyTest.java
@@ -0,0 +1,90 @@
+/**********************************************************************
+Copyright (c) 2012 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**********************************************************************/
+package com.google.appengine.datanucleus.jpa;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.datanucleus.util.NucleusLogger;
+
+import com.google.appengine.datanucleus.test.HasManyToManyAJPA;
+import com.google.appengine.datanucleus.test.HasManyToManyBJPA;
+
+/**
+ * Tests for unowned M-N relations
+ */
+public class JPAManyToManyTest extends JPATestCase {
+
+  public void testPersist() throws Exception {
+    EntityManager em = emf.createEntityManager();
+    try {
+      // Persist Side A
+      HasManyToManyBJPA b = new HasManyToManyBJPA();
+      em.persist(b);
+
+      // Create and persist Side B with another Side A, plus the existing Side A
+      HasManyToManyAJPA a = new HasManyToManyAJPA();
+      HasManyToManyBJPA b2 = new HasManyToManyBJPA();
+      b2.getAs().add(a);
+      a.getBs().add(b2);
+      a.getBs().add(b);
+      b.getAs().add(a);
+      em.persist(b2);
+      em.persist(a);
+    } catch (Exception e) {
+      NucleusLogger.GENERAL.error("Exception in persist", e);
+      fail("Exception in test : " + e.getMessage());
+      return;
+    } finally {
+      if (em.getTransaction().isActive()) {
+        em.getTransaction().rollback();
+      }
+      em.close();
+    }
+    if (emf.getCache() != null) {
+      emf.getCache().evictAll();
+    }
+
+    em = emf.createEntityManager();
+    try {
+      Query qa = em.createQuery("SELECT a FROM HasManyToManyAJPA a");
+      List<HasManyToManyAJPA> results1 = qa.getResultList();
+      assertNotNull(results1);
+      assertEquals("Incorrect number of side A", 1, results1.size());
+      HasManyToManyAJPA a = results1.iterator().next();
+      Set<HasManyToManyBJPA> bs = a.getBs();
+      assertNotNull(bs);
+      assertEquals("Incorrect number of side B for A", 2, bs.size());
+
+      Query qb = em.createQuery("SELECT b FROM HasManyToManyBJPA b");
+      List<HasManyToManyBJPA> results2 = qb.getResultList();
+      assertNotNull(results2);
+      assertEquals("Incorrect number of side B", 2, results2.size());
+    } catch (Exception e) {
+      NucleusLogger.GENERAL.error("Exception in retrieval/checking", e);
+      fail("Exception in test : " + e.getMessage());
+      return;
+    } finally {
+      if (em.getTransaction().isActive()) {
+        em.getTransaction().rollback();
+      }
+      em.close();
+    }
+  }
+}
