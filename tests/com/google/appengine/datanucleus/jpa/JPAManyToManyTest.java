18f9df3d02e4b3d64605c3732d8a7e42ec4d8f46
andy_jefferson <andy_jefferson@yahoo.com>
2012-02-05 09:38:59 +0000

    two other test variants

--- tests/com/google/appengine/datanucleus/jpa/JPAManyToManyTest.java
+++ tests/com/google/appengine/datanucleus/jpa/JPAManyToManyTest.java
@@ -31,14 +31,14 @@ import com.google.appengine.datanucleus.test.HasManyToManyBJPA;
  */
 public class JPAManyToManyTest extends JPATestCase {
 
-  public void testPersist() throws Exception {
+  public void testPersistBThenRelation() throws Exception {
     EntityManager em = emf.createEntityManager();
     try {
-      // Persist Side A
+      // Persist Side B
       HasManyToManyBJPA b = new HasManyToManyBJPA();
       em.persist(b);
 
-      // Create and persist Side B with another Side A, plus the existing Side A
+      // Create and persist Side A with another Side B, plus the existing Side B
       HasManyToManyAJPA a = new HasManyToManyAJPA();
       HasManyToManyBJPA b2 = new HasManyToManyBJPA();
       b2.getAs().add(a);
@@ -46,6 +46,118 @@ public class JPAManyToManyTest extends JPATestCase {
       a.getBs().add(b);
       b.getAs().add(a);
       em.persist(b2);
+      em.persist(a);
+    } catch (Exception e) {
+      NucleusLogger.GENERAL.error("Exception in persist", e);
+      fail("Exception in test : " + e.getMessage());
+      return;
+    } finally {
+      if (em.getTransaction().isActive()) {
+        em.getTransaction().rollback();
+      }
+      em.close();
+    }
+    if (emf.getCache() != null) {
+      emf.getCache().evictAll();
+    }
+
+    em = emf.createEntityManager();
+    try {
+      Query qa = em.createQuery("SELECT a FROM HasManyToManyAJPA a");
+      List<HasManyToManyAJPA> results1 = qa.getResultList();
+      assertNotNull(results1);
+      assertEquals("Incorrect number of side A", 1, results1.size());
+      HasManyToManyAJPA a = results1.iterator().next();
+      Set<HasManyToManyBJPA> bs = a.getBs();
+      assertNotNull(bs);
+      assertEquals("Incorrect number of side B for A", 2, bs.size());
+
+      Query qb = em.createQuery("SELECT b FROM HasManyToManyBJPA b");
+      List<HasManyToManyBJPA> results2 = qb.getResultList();
+      assertNotNull(results2);
+      assertEquals("Incorrect number of side B", 2, results2.size());
+    } catch (Exception e) {
+      NucleusLogger.GENERAL.error("Exception in retrieval/checking", e);
+      fail("Exception in test : " + e.getMessage());
+      return;
+    } finally {
+      if (em.getTransaction().isActive()) {
+        em.getTransaction().rollback();
+      }
+      em.close();
+    }
+  }
+
+  public void testPersistAThenRelation() throws Exception {
+    EntityManager em = emf.createEntityManager();
+    try {
+      // Persist Side A
+      HasManyToManyAJPA a = new HasManyToManyAJPA();
+      em.persist(a);
+
+      // Create and persist Side B with Side A, plus another Side B
+      HasManyToManyBJPA b = new HasManyToManyBJPA();
+      b.getAs().add(a);
+      a.getBs().add(b);
+      em.persist(b);
+
+      HasManyToManyBJPA b2 = new HasManyToManyBJPA();
+      b2.getAs().add(a);
+      a.getBs().add(b2);
+      em.persist(b2);
+    } catch (Exception e) {
+      NucleusLogger.GENERAL.error("Exception in persist", e);
+      fail("Exception in test : " + e.getMessage());
+      return;
+    } finally {
+      if (em.getTransaction().isActive()) {
+        em.getTransaction().rollback();
+      }
+      em.close();
+    }
+    if (emf.getCache() != null) {
+      emf.getCache().evictAll();
+    }
+
+    em = emf.createEntityManager();
+    try {
+      Query qa = em.createQuery("SELECT a FROM HasManyToManyAJPA a");
+      List<HasManyToManyAJPA> results1 = qa.getResultList();
+      assertNotNull(results1);
+      assertEquals("Incorrect number of side A", 1, results1.size());
+      HasManyToManyAJPA a = results1.iterator().next();
+      Set<HasManyToManyBJPA> bs = a.getBs();
+      assertNotNull(bs);
+      assertEquals("Incorrect number of side B for A", 2, bs.size());
+
+      Query qb = em.createQuery("SELECT b FROM HasManyToManyBJPA b");
+      List<HasManyToManyBJPA> results2 = qb.getResultList();
+      assertNotNull(results2);
+      assertEquals("Incorrect number of side B", 2, results2.size());
+    } catch (Exception e) {
+      NucleusLogger.GENERAL.error("Exception in retrieval/checking", e);
+      fail("Exception in test : " + e.getMessage());
+      return;
+    } finally {
+      if (em.getTransaction().isActive()) {
+        em.getTransaction().rollback();
+      }
+      em.close();
+    }
+  }
+
+  public void testPersistInOneStep() throws Exception {
+    EntityManager em = emf.createEntityManager();
+    try {
+      // Persist Side A
+      HasManyToManyBJPA b = new HasManyToManyBJPA();
+      HasManyToManyAJPA a = new HasManyToManyAJPA();
+      HasManyToManyBJPA b2 = new HasManyToManyBJPA();
+      b2.getAs().add(a);
+      a.getBs().add(b2);
+      a.getBs().add(b);
+      b.getAs().add(a);
+      em.persist(a);
       em.persist(a);
     } catch (Exception e) {
       NucleusLogger.GENERAL.error("Exception in persist", e);
