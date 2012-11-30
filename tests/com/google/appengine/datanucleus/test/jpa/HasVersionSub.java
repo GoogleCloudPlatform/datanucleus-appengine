cd146bab02fe621478f62bdae5008f76c1a23cd9
andy_jefferson <andy_jefferson@yahoo.com>
2012-11-30 08:34:29 +0000

    override equals/hashCode

--- tests/com/google/appengine/datanucleus/test/jpa/HasVersionSub.java
+++ tests/com/google/appengine/datanucleus/test/jpa/HasVersionSub.java
@@ -36,4 +36,24 @@ public class HasVersionSub implements Serializable {
     public int getValue() {
       return value;
     }
+
+    public boolean equals(Object obj) {
+      if (obj == null || !(obj instanceof HasVersionSub)) {
+        return false;
+      }
+      HasVersionSub other = (HasVersionSub)obj;
+      if (other.key == null || key == null) {
+        return false;
+      }
+      return key.equals(other.key);
+    }
+
+    public int hashCode() {
+      if (key == null) {
+        return 0;
+      }
+      return key.hashCode();
+    }
+
+    
 }
